57e49a3a3d4643beacc345e68c63ed6569fc889f
gitflow-tech <gitflow45@gmail.com>
2026-07-10 12:54:40 +0330

    Add a new p tag

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -7,6 +7,7 @@ const Greetings = () => {
             <div className="greetings-content">
                 <h1 className="title">This is a very simple test for Git Repository</h1>
                 <p className="subtitle">✨ animated border • poppins font</p>
+                <p className='subtitle'>This is a another commit test</p>
             </div>
         </div>
     );
